0dcdb58ceb37cc7f72613198a1ebb199ea221516
willianchu <willianchu@hotmail.com>
2023-03-03 13:29:03 -0800

    event cleaner

--- useEffectHook/App.js
+++ useEffectHook/App.js
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from 'react';
 
 export default function App() {
   const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    console.log('resource changed');
+    window.addEventListener('mousemove', e => {
+      console.log(e);
+    });
+
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json));
+
+    return () => {
+      window.removeEventListener('mousemove', e => {
+        console.log(e);
+      });
+    }; // cleanup function
+    
   }, [resourceType]);
-  
+
 
   return (
     <>
@@ -16,6 +30,9 @@ export default function App() {
         <button onClick={() => setResourceType('comments')}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      }
     </>
   );
 }
